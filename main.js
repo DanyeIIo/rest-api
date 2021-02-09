ef64905bdfd6bc9339fa9e82e0f87ab684ec02e4
DanyeIIo <78212648+DanyeIIo@users.noreply.github.com>
2021-02-09 17:37:57 +0600

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require('cors')
 
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect('mongodb+srv://Danyello:D22@08N03i@mydb.uxyjh.mongodb.net/MyDB?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true});
 
 const app = express();
 app.use(express.json());
